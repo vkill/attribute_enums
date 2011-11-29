50b0092a7246c890a500dd92cbe179e5e668143e
vkill <vkill.net@gmail.com>
2011-11-29 12:19:19 +0800

    modify spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,10 @@
 #encoding: utf-8
+require "rubygems"
+require "bundler/setup"
+require "pry"
 
+
+#loading rails
 ENV["RAILS_ENV"] ||= 'test'
 require "rails3_0_app/config/environment"
 ActiveRecord::Migrator.migrate(File.expand_path("../rails3_0_app/db/migrate", __FILE__))
@@ -7,19 +12,12 @@ I18n.load_path << Dir[ File.expand_path("../support/locales/**/*.{rb,yml}", __FI
 I18n.reload!
 
 
-require "rubygems"
-require "bundler/setup"
-require "attribute_enums"
-require "pry"
-
+#loading rspec
 require 'rspec/rails'
 require 'rspec/autorun'
-
 require 'capybara/rspec'
 require 'timecop'
-
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].each {|f| require f}
-
 RSpec.configure do |config|
   config.mock_with :rspec
 end
